29a09a235ad6553e66ec6ef6d76d2017072e4dce
unknown <mipcomputacion@gmail.com>
2020-10-02 22:04:34 -0300

    ejercicios-05-js-IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var gato = {
+      nombre: nombre,
+      edad : edad,
+      meow: function(){
+        return "Meow!";
+      },
+  };
+  return gato;
 }
 
 
@@ -14,6 +22,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property] = null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,12 +31,15 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]();
+
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
+  return 5*objetoMisterioso.numeroMisterioso;
 
 }
 
@@ -34,19 +47,32 @@ function eliminarPropiedad(objeto, propiedad) {
   // Elimina la propiedad "propiedad" de "objeto"
   // Devuelve el objeto
   // Tu código:
+  delete objeto[propiedad];
+  return objeto;
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var objeto = {
+     nombre: nombre,
+     email : email,
+     password: password,
+  }
+return objeto;
 }
 
 function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  let email = usuario.email;
+  if(email != null){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 
@@ -55,19 +81,35 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
-}
+  for (const property in objeto) {
+    let p = property;
+    if(p == propiedad){
+      return true;
+       }
+     }
+    return false;
+  }
+
 
 function verificarPassword(usuario, password) {
   // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  let p = usuario.password;
+  if(p === password){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -75,6 +117,9 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -83,6 +128,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+    for(let i = 0; i < usuarios.length; i++){
+        usuarios[i].esPremium = true;
+       }
+       return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -92,6 +141,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  let likes = 0;
+  for(let i = 0; i < usuario.posts.length; i++){
+    likes = likes + usuario.posts[i].likes;
+     }
+     return likes;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -104,7 +158,15 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  console.log(producto);
+ producto.calcularPrecioDescuento = function(){
+       let p = this.precio;
+       let pd = this.porcentajeDeDescuento;
+       let d = p - (p*pd);
+       return d;
+   }
+   console.log(producto);
+return producto;
 }
 
 // No modificar nada debajo de esta línea
---
+++ 05-JS-IV/homework/tests/deficiciones.md
@@ -0,0 +1,16 @@
+* Objetos:
+          Los objetos son una colección o un array en donde podemos definir las claves con la cual podremos luago acceder a los valores asociados.  veamoslos asi , podems crear un objeto  persona y darle a esa persona una propiedad nombre y un apellido y una edad supongamos. entonces decimos que nombre es igual a "juan" apellido es = "lopez" y edad sera = " 55" de esta manera cuando queremos saber su edad simplememnte nos referimos a la propiedad edad y el objeto nos devuelve "55" y asi con el nombre nos dara " juan" y en el apellido "lopez"
+
+ * Propiedades:
+               las propiedades son las claves que agregamos a los objetos, son por lo general caracteristicas que tiene     nuestros objetos que queremos modelar, si nos referimos a una persona pueden ser las propiedades "nombre",
+               "apellido", "edad" "telefono", etc; si fuera un auto podria ser capas "marca", "modelo", "color", etc.
+
+* Métodos:
+            los metodos son funciones que podemos agregar en nuestros objetos como propiedades. y podemos aceder a ellas de la misma manera.
+
+* Bucle `for…in`:recorre las propiedades de un objeto , esto quiere decir que podemos iterar al igua que un for
+         un objeto pero solo lo hara una vez y recorrera todos las propiedades del objeto y finalizara. esto es muy util ya que losobjetos no tiene indices numerados si no que tiene propiedades;
+
+*   Notación de puntos vs notación de corchetes: son dos metodos que podemos utilizar para aceder a
+                                                 los valores de las     propiedades dentro de un objeto. por lo general se
+                                                 utiliza mas el punto por su simpleza pero habra ocaciones que se usan los corchetes por lo general cuando usamos variables para referirnos a las proíedades. 
